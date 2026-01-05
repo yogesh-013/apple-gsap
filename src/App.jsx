f270a86e85fb7d49e79c9f9e538760c73ea78be3
Yogesh Paneru <yogeshpaneru56@gmail.com>
2026-01-06 01:06:20 +0530

    performance components added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
 import Showcase from './components/Showcase'
+import Performance from './components/Performance'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 function App() {
-
+gsap.registerPlugin(ScrollTrigger)
   return (
         <main >
             <NavBar />
             <Hero />
             <ProductViewer />
             <Showcase />
+            <Performance />
         </main>
     )
 }

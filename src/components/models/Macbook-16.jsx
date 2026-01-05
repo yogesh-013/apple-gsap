b75af66664049aeff1995801b09a654a27f0e586
Yogesh Paneru <yogeshpaneru56@gmail.com>
2026-01-05 23:26:15 +0530

    product-viewer model completed

--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -10,9 +10,24 @@ Title: macbook pro M3 16 inch 2024
 
 import React from 'react'
 import { useGLTF } from '@react-three/drei'
-
-export function Model(props) {
-  const { nodes, materials } = useGLTF('/macbook-16-transformed.glb')
+import { useTexture } from '@react-three/drei'
+import { noChangeParts } from '../../constants'
+import { useEffect } from 'react'
+import useMacbookStore from '../../store'
+import { Color } from 'three'
+export default function MacbookModel16(props) {
+  const texture = useTexture('./screen.png')
+  const { nodes, materials , scene} = useGLTF('/models/macbook-16-transformed.glb')
+  const color = useMacbookStore((state) => state.color);
+  useEffect(() => {
+        scene.traverse((child) => {
+            if (child.isMesh) {
+                if (!noChangeParts.includes(child.name)) {
+                    child.material.color = new Color(color);
+                }
+            }
+        });
+    }, [color]);
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Object_10.geometry} material={materials.PaletteMaterial001} rotation={[Math.PI / 2, 0, 0]} />
@@ -32,10 +47,12 @@ export function Model(props) {
       <mesh geometry={nodes.Object_82.geometry} material={materials.gMtYExgrEUqPfln} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_96.geometry} material={materials.PaletteMaterial003} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_107.geometry} material={materials.JvMFZolVCdpPqjj} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.Object_123.geometry} material={materials.sfCQkHOWyrsLmor} rotation={[Math.PI / 2, 0, 0]} />
+      <mesh geometry={nodes.Object_123.geometry} material={materials.sfCQkHOWyrsLmor} rotation={[Math.PI / 2, 0, 0]} >
+      <meshBasicMaterial map={texture} />
+      </mesh>
       <mesh geometry={nodes.Object_127.geometry} material={materials.ZCDwChwkbBfITSW} rotation={[Math.PI / 2, 0, 0]} />
     </group>
   )
 }
 
-useGLTF.preload('/macbook-16-transformed.glb')
+useGLTF.preload('/models/macbook-16-transformed.glb')
--- src/constants/index.js
+++ src/constants/index.js
@@ -6,3 +6,22 @@ export const navLinks = [
     { label: "Vision" },
     { label: "AirPods" },
 ];
+
+export const noChangeParts = [
+    "Object_84",
+    "Object_37",
+    "Object_34",
+    "Object_12",
+    "Object_80",
+    "Object_35",
+    "Object_36",
+    "Object_13",
+    "Object_125",
+    "Object_76",
+    "Object_33",
+    "Object_42",
+    "Object_58",
+    "Object_52",
+    "Object_21",
+    "Object_10",
+];

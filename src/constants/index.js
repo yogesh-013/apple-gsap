9c426f158bbfa081702a95e8c71fa463b8b05b9c
Yogesh Paneru <yogeshpaneru56@gmail.com>
2026-01-06 17:24:29 +0530

    Added footer section

--- src/constants/index.js
+++ src/constants/index.js
@@ -119,3 +119,11 @@ export const featureSequence = [
     { videoPath: "/videos/feature-4.mp4", boxClass: ".box4", delay: 0 },
     { videoPath: "/videos/feature-5.mp4", boxClass: ".box5", delay: 0 },
 ];
+
+export const footerLinks = [
+    { label: "Privacy Policy", link: "#" },
+    { label: "Terms of Use", link: "#" },
+    { label: "Sales Policy", link: "#" },
+    { label: "Legal", link: "#" },
+    { label: "Site Map", link: "#" },
+];
